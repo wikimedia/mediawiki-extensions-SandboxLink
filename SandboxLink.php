b7c4c955f3284c29b538dca82d2fb3aad1742d8c
Bartosz Dziewoński <matma.rex@gmail.com>
2014-08-29 22:59:06 +0200

    Initial extension code
    Bug: 69650
    Change-Id: Idace81fc12d10cf4581f0e6fe90ba08f31c79eef

---
+++ SandboxLink.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Add a link to user's personal sandbox to personal tools menu.
+ *
+ * https://www.mediawiki.org/wiki/Extension:SandboxLink
+ *
+ * @file
+ * @license MIT
+ */
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'SandboxLink',
+	'author' => array(
+		'Bartosz Dziewoński',
+	),
+	'url' => 'https://www.mediawiki.org/wiki/Extension:SandboxLink',
+	'descriptionmsg' => 'sandboxlink-desc',
+);
+
+$wgMessagesDirs['SandboxLink'] = __DIR__ . '/i18n';
+$wgAutoloadClasses['SandboxLinkHooks'] = __DIR__ . '/SandboxLinkHooks.php';
+
+$wgHooks['SkinPreloadExistence'][] = 'SandboxLinkHooks::onSkinPreloadExistence';
+$wgHooks['PersonalUrls'][] = 'SandboxLinkHooks::onPersonalUrls';
+
+// Configuration settings
+
+// Whether to disable sandbox link functionality for anonymous users.
+$wgSandboxLinkDisableAnon = true;
