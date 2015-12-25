33dffa90d7a906935f3e2c1039a2d23a0c092ca0
ananay <i@ananayarora.com>
2015-12-25 23:57:30 +0530

    Convert SandboxLink to use extension registration
    Bug: T122094
    Change-Id: I3906d45baf587626884f1977b7d896c79d8aa61d

--- SandboxLink.php
+++ SandboxLink.php
@@ -7,25 +7,9 @@
  * @file
  * @license MIT
  */
-
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'SandboxLink',
-	'author' => array(
-		'Bartosz Dziewoński',
-	),
-	'url' => 'https://www.mediawiki.org/wiki/Extension:SandboxLink',
-	'descriptionmsg' => 'sandboxlink-desc',
-	'license-name' => 'MIT',
-);
-
-$wgMessagesDirs['SandboxLink'] = __DIR__ . '/i18n';
-$wgAutoloadClasses['SandboxLinkHooks'] = __DIR__ . '/SandboxLinkHooks.php';
-
-$wgHooks['SkinPreloadExistence'][] = 'SandboxLinkHooks::onSkinPreloadExistence';
-$wgHooks['PersonalUrls'][] = 'SandboxLinkHooks::onPersonalUrls';
-
-// Configuration settings
-
-// Whether to disable sandbox link functionality for anonymous users.
-$wgSandboxLinkDisableAnon = true;
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'SandboxLink' );
+	$wgMessagesDirs['SandboxLink'] = __DIR__ . '/i18n';
+} else {
+	die( 'This version of the SandboxLink extension requires MediaWiki 1.25+' );
+}
