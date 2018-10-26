3731cace8c924e758486d1111b62cf3879a56e45
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:52:41 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I234d9f781c726a1ffa8262edc26ad17eebd9aebb

--- SandboxLink.php
+++ SandboxLink.php
@@ -10,6 +10,10 @@
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'SandboxLink' );
 	$wgMessagesDirs['SandboxLink'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for SandboxLink extension. Please use wfLoadExtension instead,' .
+		' see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 } else {
 	die( 'This version of the SandboxLink extension requires MediaWiki 1.25+' );
 }
